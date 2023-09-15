f2f2a69b9967fae9e28ecc0e911fc9f9bc6d20b9
BROWN-BEN <benjamin.brown6@griffithuni.edu.au>
2023-09-15 21:01:36 +1000

    Code works now

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,20 @@
 import matplotlib.pyplot as plt
+
+import sqlite3
+
+con = sqlite3.connect("climate.db")
+cur = con.cursor()
+
+for row in cur.execute("SELECT * FROM ClimateData"):
+    print(row)
+
+years = list(cur.execute("SELECT year FROM ClimateData"))
+co2 = list(cur.execute("SELECT CO2 FROM ClimateData"))
+temp = list(cur.execute("SELECT temperature FROM ClimateData"))
         
-years = []
-co2 = []
-temp = []
+#years = []
+#co2 = []
+#temp = []
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
